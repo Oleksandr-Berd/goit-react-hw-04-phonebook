0787816599e2027276602a602bd9079de821971b
Oleksandr <105509345+Oleksandr-Berd@users.noreply.github.com>
2022-11-08 21:48:28 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,21 +3,12 @@ import Form from './Form/Form';
 import ContactList from './ContactList/ContactList';
 import Filter from './Filter/Filter';
 import css from './App.module.css';
-import { useEffect, useState } from 'react';
+import { useState, useMemo } from 'react';
 import useLocalStorage from '../Hooks/useLokalStorage';
 
 export default function App() {
   const [contacts, setContacts] = useLocalStorage('contacts', []);
   const [filterQuery, setFilterQuery] = useState('');
-  // const [contacts, setContacts] = useState([]);
-
-  // useEffect(() => {
-  //   if (contacts.some(contact => contact.name === name)) {
-  //     alert(`contact ${name} already in contacts`);
-  //     return;
-  //   }
-  //   setContacts(state => [...state, { name, number }]);
-  // }, [contacts, name, number, setContacts]);
 
   const formSubmitHandler = ({ name, number }) => {
     if (contacts.some(contact => contact.name === name)) {
@@ -37,26 +28,13 @@ export default function App() {
     setFilterQuery(evt.currentTarget.value);
   };
 
-  // useEffect(() => {
-  //   const normilizedFilter = filterQuery.toLowerCase();
-  //   if (normilizedFilter !== '') {
-  //     setContacts(prevState =>
-  //       prevState.filter(contact =>
-  //         contact.name.toLowerCase().includes(normilizedFilter)
-  //       )
-  //     );
-  //   }
-  // }, [contacts, filterQuery, setContacts]);
-
-  // const getVisibleContacts = () => {
-  //   const normilizedFilter = filterQuery.toLowerCase();
-  //   console.log(contacts);
-  //   return contacts.filter(contact =>
-  //     contact.name.toLowerCase().includes(normilizedFilter)
-  //   );
-
-  //   return contacts;
-  // };
+  const getVisibleContacts = useMemo(() => {
+    const normilizedFilter = filterQuery.toLowerCase();
+    console.log(contacts);
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normilizedFilter)
+    );
+  }, [contacts, filterQuery]);
 
   return (
     <div className={css.container}>
@@ -66,9 +44,7 @@ export default function App() {
       <h2 className={css.contactList}>Contacts</h2>
       {contacts.length > 0 && (
         <ContactList
-          contacts={contacts.filter(contact =>
-            contact.name.toLowerCase().includes(filterQuery.toLowerCase())
-          )}
+          contacts={getVisibleContacts}
           onDeleteContact={deleteContact}
         />
       )}
